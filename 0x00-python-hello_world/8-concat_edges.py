fcb44a7deafddea7c7ad2a471707647847e40741
OzimiWhoke <ozimi11zz@gmail.com>
2023-01-04 12:02:57 -0800

    Hello World in Python

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
-#!/usr/bash/python3
+#!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
         language that combines remarkable power with very clear syntax"
-str = str[39:65] + str[104:107] + str[0:5]
+        str = str[39:67] + str[107:112] + str[:6]
         print(str)
