4aac909b4bec0dcce8537437283b0b5dd63cb627
OzimiWhoke <ozimi11zz@gmail.com>
2023-01-11 17:53:37 -0800

    Updates on lists and tuples

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence):
-        if length == 0:
-            result = (0, None)
-            return result
-        else:
-            res = (length, sentence[0:1])
-    return res
+    n = len(sentence)
+    if n == 0:
+        return (0, None)
+    return (n, sentence[0])
