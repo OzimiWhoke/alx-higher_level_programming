6bb7028b0a48026cd99b216c25e941b3be2c10ca
OzimiWhoke <ozimi11zz@gmail.com>
2023-01-13 15:57:24 -0800

    a function that returns a new dictionary with all values multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dictionary = {}
-    for k, v in a_dictionary.items():
-        new_dictionary[key] = v * 2
+    for key, value in a_dictionary.items():
+        new_dictionary[key] = value * 2
     return new_dictionary
