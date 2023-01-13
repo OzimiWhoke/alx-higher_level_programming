0208d8468b83f5b3ef28acd5daa0eb201c632dec
OzimiWhoke <ozimi11zz@gmail.com>
2023-01-13 15:16:53 -0800

    a function that returns the number of keys in a dictionary

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    person = {"name": "whoke", "age": 24, "town": "warri"}
-    for i in person.key();
-    print(i)
+    return (len(a_dictionary.keys()))
