9725c75aed2d6c333e9fa7397a83be298a6fb269
OzimiWhoke <ozimi11zz@gmail.com>
2023-01-14 19:12:39 -0800

    update on python

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary and len(a_dictionary):
-        max = list(a_dictionary.keys())[0]
-        for key in a_dictionary:
-            if a_dictionary[key] > a_dictionary[max]
-            max = key
-        return max
-    return None
+    if a_dictionary is None:
+        return None
+    if len(a_dictionary.values()) == 0:
+        return None
+    max_value = max(a_dictionary.values())
+    for key, value in a_dictionary.items():
+        if value == max_value:
+            return key 
