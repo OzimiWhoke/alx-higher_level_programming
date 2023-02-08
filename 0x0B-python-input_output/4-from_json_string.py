44899992ea4d79e7a30438e83613edeb6aa3b342
OzimiWhoke <ozimi11zz@gmail.com>
2023-02-07 18:54:21 -0800

    Python I/O

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -3,6 +3,9 @@
 contains the json string function
 """
 
+import json
+
 
 def from_json_string(my_str):
+    """ returns an object represented by a JSON string"""
     return json.loads(my_str)
--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,4 +5,6 @@ contains JSON class function"
 
 
 def class_to_json(obj):
+    """returns the dictionary description with simple data structure
+    for JSON serialization of an object"""
     return obj.__dict__
