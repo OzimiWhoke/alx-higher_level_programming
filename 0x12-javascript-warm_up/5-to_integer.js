491be0dcdf3db2197b83dc2b8d21d260ce5998f5
OzimiWhoke <ozimi11zz@gmail.com>
2023-04-13 15:47:36 -0700

    Argument

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const Num_ber = Math.floor(Number(process.argv[2]));
-console.log(isNaN(num) ? 'Not a number' : `My number: ${Num_ber}`);i
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);i
