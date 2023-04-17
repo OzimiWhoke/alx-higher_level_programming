dad7530163e9f5b8b52923e226a43aa576e0b93c
OzimiWhoke <ozimi11zz@gmail.com>
2023-04-17 15:23:34 -0700

    Occurence

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,21 +1,13 @@
 #!/usr/bin/node
-// Import Square class from 5-square.js
 const BaseSquare = require('./5-square');
-
-// Define Square class that inherits from BaseSquare
 class Square extends BaseSquare {
   charPrint(c) {
-    // Set default character to X if c is undefined
     if (!c) {
       c = 'X';
     }
-
-    // Print square using specified character
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
     }
   }
 }
-
-// Export Square class
 module.exports = Square;
