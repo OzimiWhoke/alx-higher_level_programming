47c33ae396af79f66acf4aaa5ee69d049627d850
OzimiWhoke <ozimi11zz@gmail.com>
2023-04-17 15:57:36 -0700

    Concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,21 @@
 #!/usr/bin/node
-#!/usr/bin/node
 const fs = require('fs');
-const a = fs.readFileSync(process.argv[2], 'utf8');
-const b = fs.readFileSync(process.argv[3], 'utf8');
-fs.writeFileSync(process.argv[4], a + b);
+
+if (process.argv.length < 5) {
+  console.log('Usage: node concat.js <file1> <file2> <output>');
+  process.exit(1);
+}
+
+const file1 = process.argv[2];
+const file2 = process.argv[3];
+const output = process.argv[4];
+
+try {
+  const data1 = fs.readFileSync(file1, 'utf8');
+  const data2 = fs.readFileSync(file2, 'utf8');
+  fs.writeFileSync(output, data1 + data2);
+  console.log(`Concatenated ${file1} and ${file2} into ${output}`);
+} catch (err) {
+  console.error('Error:', err);
+}
+
