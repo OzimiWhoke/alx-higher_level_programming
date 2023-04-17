8b04ad3fe1ed2fc279a89946e4b6b5f0c00cab0e
OzimiWhoke <ozimi11zz@gmail.com>
2023-04-17 15:47:02 -0700

    Importing an array for computation

--- 0x13-javascript_objects_scopes_closures/100-data.js
+++ 0x13-javascript_objects_scopes_closures/100-data.js
@@ -1,3 +1,2 @@
 #!/usr/bin/node
-const list = [1, 2, 3, 4, 5];
-module.exports = list;
+expoers.list = [1, 2, 3, 4, 5];
--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-const list = require('./100-data.js');
-
-const newList = list.map((value, index) => {
-  return value * index;
-});
-
+const list = require('./100-data.js').list;
 console.log(list);
-console.log(newList);
+console.log(list.map((item, index) => item * index));
