a04d09e907682247a29ca3620ac86277bc92ea8e
OzimiWhoke <ozimi11zz@gmail.com>
2023-04-17 14:57:59 -0700

    Class Rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
-class Rectangle:
-    def __init__(self, w, h):
-        if w <= 0 or h <= 0:
-            pass  # creating an empty object
-        else:
-            self.width = w
-            self.height = h
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
 
-    def print(self):
-        if hasattr(self, 'width') and hasattr(self, 'height'):
-            for i in range(self.height):
-                for j in range(self.width):
-                    print("X", end="")
-                print()
-module.exports = Rectangle;
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  }
+};
