8c3947c8acf15085a8b2f19b74bdb915c2985072
OzimiWhoke <ozimi11zz@gmail.com>
2023-05-01 23:34:58 -0700

    making a request causing the server to respond with a message

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me! using curl
+
+curl -s -X PUT -L -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
