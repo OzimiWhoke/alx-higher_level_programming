70d0de28d5127335488d41b6f159582ce250f5c5
OzimiWhoke <ozimi11zz@gmail.com>
2023-05-23 16:16:06 -0700

    JSON

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,18 +1,6 @@
 #!/usr/bin/node
 
-import sys
-
-def write_to_file(file_path, content):
-    try:
-        with open(file_path, 'w', encoding='utf-8') as file:
-            file.write(content)
-            print("Content successfully written to the file.")
-    except Exception as e:
-        print(f"An error occurred while writing to the file: {e}")
-
-if len(sys.argv) < 3:
-    print("Please provide the file path and content as arguments.")
-else:
-    file_path = sys.argv[1]
-    content = sys.argv[2]
-    write_to_file(file_path, content)
+const fs = require('fs');
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
+});
